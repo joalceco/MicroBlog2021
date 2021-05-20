91fe5b833e3462968aedd2cb7d1b3725318f91a0
Jorge Cerecedo <joalceco@gmail.com>
2021-05-20 17:45:05 -0500

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from app import app
+from flask import render_template
+
+@app.route("/")
+@app.route("/index")
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Jorge'},
+            'body': 'Hola UNE!'
+        }
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
+
